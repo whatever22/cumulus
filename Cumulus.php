0f55d727a1e0439a10d05dab04ec7f20d4947f5b
Mathias Chouet <mathias@tela-botanica.org>
2015-07-17 14:04:41 +0200

    Ajout d'une config Gestion de la BD avec PDO Ajout d'un .htaccess (rewrite)

---
+++ Cumulus.php
@@ -0,0 +1,216 @@
+<?php
+
+/**
+ * API REST pour le stockage de fichiers
+ */
+class Cumulus {
+
+	/** Base de données PDO */
+	protected $db;
+
+	/** Config parsée depuis le fichier config/config.json */
+	protected $config = array();
+	public static $CHEMIN_CONFIG = "config/config.json";
+
+	/** HTTP verb received (GET, POST, PUT, DELETE, OPTIONS) */
+	protected $verb;
+
+	/** Paramètres reçus */
+	protected $params = array();
+
+	/** Clef reçue */
+	protected $key;
+
+	/** URL de base pour parser les éléments (ressources) */
+	protected $baseURL;
+
+	public function __construct() {
+		// config
+		if (file_exists(self::$CHEMIN_CONFIG)) {
+			$this->config = json_decode(file_get_contents(self::$CHEMIN_CONFIG), true);
+		} else {
+			throw new Exception("Le fichier config/config.json n'existe pas");
+		}
+
+		// database
+		$DB = $this->config['db'];
+		$dsn = "mysql:host=" . $DB['host'] . ";dbname=" . $DB['dbname'] . ";port=" . $DB['port'];
+		$this->db = new PDO($dsn, $DB['username'], $DB['password']);
+
+		// méthode HTTP
+		$this->verb = $_SERVER['REQUEST_METHOD'];
+		//echo "Method: " . $this->verb . PHP_EOL;
+
+		// @TODO read from config
+		$this->baseURL = $this->config['baseurl'];
+		echo "Base URL: " . $this->baseURL . PHP_EOL;
+
+		$this->init();
+		$this->getParams(); // @TODO maybe move to init() ?
+		print_r($this->params);
+
+		$this->run();
+	}
+
+	/** Post-constructor adjustments */
+	protected function init() {
+	}
+
+	/** Reads the request and runs the appropriate method */
+	protected function run() {
+		switch($this->verb) {
+			case "GET":
+				$this->get();
+				break;
+			case "POST":
+				$this->post();
+				break;
+			case "PUT":
+				$this->put();
+				break;
+			case "DELETE":
+				$this->delete();
+				break;
+			case "OPTIONS":
+				$this->options();
+				break;
+			default:
+				http_response_code(500);
+				echo "unrecognized method: $this->verb" . PHP_EOL;
+		}
+	}
+
+	/** Compares the request URI to the base URL to detect parameters */
+	protected function getParams() {
+		$uri = $_SERVER['REQUEST_URI'];
+		echo "URI: " . $uri . PHP_EOL;
+		if ((strlen($uri) > strlen($this->baseURL)) && (strpos($uri, $this->baseURL) !== false)) {
+			//echo "URI plus!" . PHP_EOL;
+			$params = substr($uri, strlen($this->baseURL));
+			//echo "Params: $params" . PHP_EOL;
+			$this->params = explode("/", $params);
+		}
+	}
+
+	/** Detects which way the user wants to retreive file(s) */
+	protected function get() {
+		if (empty($this->params[0])) {
+			http_response_code(404);
+			return false;
+		}
+		$param1 = $this->params[0];
+		echo "Get param 1: $param1" . PHP_EOL;
+
+		// is it a raw GET or a search ?
+		switch($param1) {
+			case "by-keyword":
+				$this->getByKeyword();
+				break;
+			case "by-name":
+				$this->getByName();
+				break;
+			case "search":
+				$this->search();
+				break;
+			default:
+				$this->getByKey($param1);
+		}
+	}
+
+	/** Retreives a file from the stock and sends it */
+	protected function getByKey($key) {
+		$q = "SELECT * FROM stor WHERE k='$key'";
+		// @TODO access rights
+		// ...
+		$result = $this->db->query($q)->fetchAll(PDO::FETCH_ASSOC);
+		if (empty($result)) {
+			echo "key not found";
+			http_response_code(404);
+			return false;
+		}
+		echo json_encode($result);
+		// @TODO send file contents to stdout
+	}
+
+	/**
+	 * Searches (public) files by name and sends a list
+	 * @TODO paginate, sort and limit
+	 */
+	protected function getByName() {
+		if (empty($this->params[1])) {
+			http_response_code(404);
+			echo "no name specified";
+			return false;
+		}
+		$name = $this->params[1];
+
+		$q = "SELECT * FROM stor WHERE name LIKE '%$name%'";
+		// @TODO access rights
+		// ...
+		$result = $this->db->query($q)->fetchAll(PDO::FETCH_ASSOC);
+		if (empty($result)) {
+			echo "no results";
+			http_response_code(404);
+			return false;
+		}
+		echo json_encode($result);
+		// @TODO generate proper list with links
+	}
+
+	/**
+	 * GET http://a.org/stor.php/by-keywords/foo
+	 * GET http://a.org/stor.php/by-keywords/foo,bar,couscous
+ 	 * GET http://a.org/stor.php/by-keywords/foo,bar,couscous/AND (default)
+ 	 * GET http://a.org/stor.php/by-keywords/foo,bar,couscous/OR
+ 	 *
+	 * Searches (public) files by keywords and sends a list
+	 * @TODO paginate, sort and limit
+	 */
+	protected function getByKeywords() {
+		// keywords
+		if (empty($this->params[1])) {
+			http_response_code(404);
+			echo "no keyword specified";
+			return false;
+		}
+		$keywords = $this->params[1];
+		$keywords = explode(",", $keywords);
+		// clause mode (OR or AND)
+		$mode = "AND";
+		if (! empty($this->params[1])) {
+			http_response_code(404);
+			echo "no keyword specified";
+			return false;
+		}
+		$clause = implode(" OR ");
+
+		$q = "SELECT * FROM stor WHERE name LIKE '%$name%'";
+		// @TODO access rights
+		// ...
+		$result = $this->db->query($q)->fetchAll(PDO::FETCH_ASSOC);
+		if (empty($result)) {
+			echo "no results";
+			http_response_code(404);
+			return false;
+		}
+		echo json_encode($result);
+		// @TODO generate proper list with links
+	}
+
+	/** Adds a file to the stock and sends the random generated key associated to it */
+	protected function put() {
+		//$key = sha2(microtime()); // @TODO test reliability
+		$key = md5(microtime());
+		echo $key;
+	}
+
+	/** Replaces an existing file with a new one, using the key */
+	protected function post() {
+		if (empty($this->params[0])) {
+			http_response_code(403);
+			echo "missing key" . PHP_EOL;
+			return false;
+		}
+		$key = $this->params[0];
+	}
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Webservice - accepte GET, PUT, POST, DELETE, OPTIONS
+ *
+ *
+ */
+
+require 'Cumulus.php';
+
+// initializing service
+$svc = new Cumulus();
