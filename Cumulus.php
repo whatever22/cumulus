b567b8070178b33146ad9ed5209fd24b583ed824
Mathias Chouet <mathias@tela-botanica.org>
2015-07-20 17:12:50 +0200

    Déplacement de la BD dans l'adapteur StockageTB Réorganisation de la config

--- Cumulus.php
+++ Cumulus.php
@@ -4,12 +4,10 @@ require 'CumulusInterface.php';
 
 /**
  * Bibliothèque pour le stockage de fichiers ("cloud") - API : fournit les
- * méthodes de haut niveau correspondant aux cas d'utilisation
+ * méthodes de haut niveau correspondant aux cas d'utilisation, et transmets
+ * les appels à l'adapteur choisi dans la config
  */
 class Cumulus implements CumulusInterface {
-
-	/** Base de données PDO */
-	protected $db;
 
 	/** Config en JSON */
 	protected $config = array();
@@ -26,11 +24,6 @@ class Cumulus implements CumulusInterface {
 			throw new Exception("Le fichier " . self::$CHEMIN_CONFIG . " n'existe pas");
 		}
 
-		// base de données
-		$DB = $this->config['db'];
-		$dsn = "mysql:host=" . $DB['host'] . ";dbname=" . $DB['dbname'] . ";port=" . $DB['port'];
-		$this->db = new PDO($dsn, $DB['username'], $DB['password']);
-
 		// adapteur
 		$adapterName = $this->config['adapter'];
 		$adapterPath = 'adapters/' . $adapterName . '.php';
@@ -38,16 +31,144 @@ class Cumulus implements CumulusInterface {
 			throw new Exception ("L'adapteur " . $adapterPath . " n'existe pas");
 		}
 		require $adapterPath;
-		$this->adapter = new $adapterName();
+		// on passe la config à l'adapteur - à lui de stocker ses paramètres
+		// dans un endroit correct (adapters.nomdeladapteur par exemple)
+		$this->adapter = new $adapterName($this->config);
 	}
 
-	public function getByKey($key) {
-		return $this->adapter->getByKey($key);
+	/**
+	 * Retourne un fichier à partir de sa clef et son chemin
+	 * @param type $path
+	 * @param type $key
+	 */
+	public function getByKey($path, $key) {
+		return $this->adapter->getByKey($path, $key);
 	}
 
-	public function getByName() {
+	/**
+	 * Retourne une liste de fichiers dont les noms correspondent à $name; si
+	 * $trict est true, compare avec un "=" sinon avec un "LIKE"
+	 * @param type $name
+	 * @param type $strict
+	 */
+	public function getByName($name, $strict=false) {
+		return $this->adapter->getByName($name, $strict);
 	}
 
-	public function getByKeywords() {
+	/**
+	 * Retourne une liste de fichiers se trouvant dans le répertoire $path; si
+	 * $recursive est true, cherchera dans tous les sous-répertoires
+	 * @param type $path
+	 * @param type $recursive
+	 */
+	public function getByPath($path, $recursive=false) {
+		return $this->adapter->getByPath($path, $recursive);
+	}
+
+	/**
+	 * Retourne une liste de fichiers dont les mots-clefs sont $keywords
+	 * (séparés par des virgules); si $mode est "OR", un "OU" sera appliqué
+	 * entre les mots-clefs, sinon un "ET"
+	 * @param type $keywords
+	 * @param type $mode
+	 */
+	public function getByKeywords($keywords, $mode="AND") {
+		return $this->adapter->getByKeywords($keywords, $mode);
+	}
+
+	/**
+	 * Retourne une liste de fichiers appartenant à l'utilisateur $user
+	 * @TODO gérer les droits
+	 * @param type $user
+	 */
+	public function getByUser($user) {
+		return $this->adapter->getByUser($user);
+	}
+
+	/**
+	 * Retourne une liste de fichiers appartenant au groupe $group
+	 * @TODO gérer les droits
+	 * @param type $group
+	 */
+	public function getByGroup($group) {
+		return $this->adapter->getByGroup($group);
+	}
+
+	/**
+	 * Retourne une liste de fichiers dont le type MIME est $mimetype
+	 * @param type $mimetype
+	 */
+	public function getByMimetype($mimetype) {
+		return $this->adapter->getByMimetype($mimetype);
+	}
+
+	/**
+	 * Retourne une liste de fichiers en fonction de leur date (@TODO de création
+	 * ou de modification ?) : si $date1 et $date2 sont spécifiées, renverra les
+	 * fichiers dont la date se trouve entre les deux; sinon, comparera à $date1
+	 * en fonction de $operator ("=", "<" ou ">")
+	 * @param type $date1
+	 * @param type $date2
+	 * @param type $operator
+	 */
+	public function getByDate($date1, $date2, $operator="=") {
+		return $this->adapter->getByDate($date1, $date2, $operator);
+	}
+
+	/**
+	 * Recherche avancée - retourne une liste de fichiers correspondant aux
+	 * critères de recherche contenus dans $searchParams (paires de
+	 * clefs / valeurs); le critère "mode" peut valoir "OR" (par défaut) ou
+	 * "AND"
+	 * @param type $searchParams
+	 */
+	public function search($searchParams=array()) {
+		return $this->adapter->search($searchParams);
+	}
+
+	/**
+	 * Ajoute le fichier $file au stock, dans le chemin $path, avec la clef $key,
+	 * les mots-clefs $keywords (séparés par des virgules) et les métadonnées
+	 * $meta (portion de JSON libre); si $key est null, une clef sera attribuée
+	 * @param type $file
+	 * @param type $path
+	 * @param type $key
+	 * @param type $keywords
+	 * @param type $meta
+	 */
+	public function addFile($file, $path, $key=null, $keywords=null, $meta=null) {
+		return $this->adapter->addFile($file, $path, $key, $keywords, $meta);
+	}
+
+	/**
+	 * Remplace le contenu (si $file est spécifié) et / ou les métadonnées du
+	 * fichier $key situé dans $path
+	 * @param type $file
+	 * @param type $path
+	 * @param type $key
+	 * @param type $keywords
+	 * @param type $meta
+	 */
+	public function updateByKey($file, $path, $key, $keywords=null, $meta=null) {
+		return $this->adapter->updateByKey($file, $path, $key, $keywords, $meta);
+	}
+
+	/**
+	 * Supprime le fichier $key situé dans $path
+	 * @param type $path
+	 * @param type $key
+	 */
+	public function deleteByKey($path, $key) {
+		return $this->adapter->deleteByKey($path, $key);
+	}
+
+	/**
+	 * Retourne les attributs (métadonnées) du fichier $key situé dans $path,
+	 * mais pas le fichier lui-même
+	 * @param type $path
+	 * @param type $key
+	 */
+	public function getAttributesByKey($path, $key) {
+		return $this->adapter->getAttributesByKey($path, $key);
 	}
 }
